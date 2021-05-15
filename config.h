a934927cc8e62c93ac34ef4ad30e29fe027ffdf4
ilightwas <6960676+ilightwas@users.noreply.github.com>
2021-05-15 00:04:11 -0300

    Hardcoded fonts for now

--- config.h
+++ config.h
@@ -5,14 +5,15 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "stfont:size=10:antialias=true:hinting=true:hintstyle=hintslight";
+static char *font = "JetBrainsMonoNL Nerd Font:size=10:antialias=true:hinting=true:hintstyle=hintslight";
 /* Spare fonts */
 static char *font2[] = {
 	"JetBrainsMonoNL Nerd Font:size=10:antialias=true:hinting=true:hintstyle=hintslight",
+	"Noto Sans Mono:size10:antialias=true:hinting=true:hintstyle=hintslight",
+	"Noto Color Emoji:size10:antialias=true:hinting=true:hintstyle=hintslight",
 	"Font Awesome 5 Free:size=10:antialias=true:hinting=true:hintstyle=hintslight",
 	"Font Awesome 5 Free Brands:size=10:antialias=true:hinting=true:hintstyle=hintslight",
 	"Font Awesome 5 Free Solid:size=10:antialias=true:hinting=true:hintstyle=hintslight",
-	"Noto Sans Mono:size10:antialias=true:hinting=true:hintstyle=hintslight"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
